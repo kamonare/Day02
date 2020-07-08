78cc602a3211bd595854eaff253134d4784f37fc
Kabelo Monareng <kamonare@c5r9s5.wethinkcode.co.za>
2020-07-08 15:52:03 +0200

    finesse

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                    /goinfre/kamonare/Desktop/Day02/ex01/ft_print_reverse_alphabet.c        */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamonare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 11:16:14 by kamonare          #+#    #+#             */
+/*   Updated: 2020/07/08 13:48:52 by kamonare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(void);
+	{
+		write(1,&c, 1);
+	}
+ 
+ void ft_putchar_alphabet(void);
+	{
+		char letter ='a';
+
+		while(letter <='z')
+		{
+			ft_putchar(letter);
+			letter==;
+		}
+		return(0)
+	}
